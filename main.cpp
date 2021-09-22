6e4ea1b9bd9b3eee883457fff8eade4fc25ab082
AnBenLa <anton.benjamin.lammert@uni-weimar.de>
2021-09-22 16:47:14 +0200

    Added directories to gitignore. Modified example loop to match tutorial from https://zeromq.org/get-started/?language=cpp&library=cppzmq#

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <string>
 #include <iostream>
 #include <zmq.hpp>
+#include <thread>
+#include <chrono>
 
 int main() {
     std::cout << "Test\n";
     zmq::context_t ctx;
-    zmq::socket_t sock(ctx, zmq::socket_type::push);
-    sock.bind("inproc://test");
-    const std::string_view m = "Hello, world";
-    sock.send(zmq::buffer(m), zmq::send_flags::dontwait);
-    std::cout << "Test2\n";
+    zmq::socket_t socket(ctx, zmq::socket_type::rep);
+    socket.bind("tcp://*:12345");
+
+    // prepare some static data for responses
+    const std::string data{"World"};
+
+    while(true){
+        zmq::message_t request;
+
+        // receive a request from client
+        socket.recv(request, zmq::recv_flags::none);
+        std::cout << "Received " << request.to_string() << std::endl;
+
+        // simulate work
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // send the reply to the client
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+    }
+
+    return 0;
 }
